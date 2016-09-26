0017fadd0e3a37e46a9436d26e92d794888c4857
Georgia Leguem <AppEduFun@mac-mini.local>
2016-09-26 17:30:46 +0200

    Initial Commit

---
+++ Focus BirmanieTests/Focus_BirmanieTests.swift
@@ -0,0 +1,36 @@
+//
+//  Focus_BirmanieTests.swift
+//  Focus BirmanieTests
+//
+//  Created by Georgia Leguem on 26/09/2016.
+//  Copyright © 2016 appedufun. All rights reserved.
+//
+
+import XCTest
+@testable import Focus_Birmanie
+
+class Focus_BirmanieTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
