f1d34e6e1e5712251ae355f5a85820ed6929eccc
laurent aubourg <laurent.aubourg@free.fr>
2018-01-30 16:22:21 +0100

    2901

--- Focus Birmanie/Slider.swift
+++ Focus Birmanie/Slider.swift
@@ -1,3 +1,13 @@
+//
+//  Sac.swift
+//  Focus Birmanie
+//
+//  Created by Laurent Aubourg on 05/04/2017.
+//  Copyright © 2017 appedufun. All rights reserved.
+//
+//
+
+
 import Foundation
 import GameplayKit
 import SpriteKit
